1223fbceabcb6a94586fa3d41b9d3f6cade7e0c3
Dekeuwer Laurent <millenium59170@gmail.com>
2019-03-25 12:40:14 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+export class Pizza {
+  id: number;
+  name: string;
+  price: number;
+}
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{pizza.name}}</h2>
+    <div><label>id: </label>{{pizza.id}}</div>
+    <div><label>name: </label>{{pizza.name}}</div>
+    <div><label>price: </label>{{pizza.price}}</div>
+  `,
 })
 export class AppComponent {
-  title = 'pizzaparty';
+  title = 'Mon site';
+  name = '4 fromages';
+  pizza: Pizza = {
+    id: 1,
+    name: 'Reine',
+    price: 12.99
+  }
 }
